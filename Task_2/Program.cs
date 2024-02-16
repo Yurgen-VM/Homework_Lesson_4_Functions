768284abf1aaadb9f0dd4afc9a24184881b6595f
Yuriy Mikhaylov <yuriy_vm@inbox.ru>
2024-02-16 13:56:57 +0300

    deleted the null strings locals

--- Task_2/Program.cs
+++ Task_2/Program.cs
@@ -8,10 +8,10 @@ void Main()
 {
     Console.Write("Введите длину массива: ");
     int size = Convert.ToInt32(Console.ReadLine());
-  
+
     int[] array = GenerateArray(size);
     int count = 0;
-    for (int i = 0; i <array.Length; i++)
+    for (int i = 0; i < array.Length; i++)
     {
         if (array[i] % 2 == 0)
         {
@@ -20,25 +20,21 @@ void Main()
     }
     Console.WriteLine($"\nКоличество четных чисел = {count}");
 
-
-
-  
-
     int[] GenerateArray(int arraySize)
     {
         int[] myArray = new int[arraySize];
         Random random = new Random();
         for (int i = 0; i < arraySize; i++)
         {
-            int randomNumber = random.Next(100,1000);
+            int randomNumber = random.Next(100, 1000);
             myArray[i] = randomNumber;
         }
         for (int i = 0; i < arraySize; i++)
         {
             Console.Write($"{myArray[i]} ");
         }
-        return myArray;   
-    }    
+        return myArray;
+    }
 }
 
 Main();
