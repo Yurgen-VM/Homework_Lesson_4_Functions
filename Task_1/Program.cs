400faf59324bbdfb638e5dcc2d35eebf1ca2322e
Yuriy Mikhaylov <yuriy_vm@inbox.ru>
2024-02-16 14:05:33 +0300

    Added Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,23 @@
+﻿/*
+Задача:
+Написать программу, которая бесконечно запрашивает целые числа с консоли.
+Программа завершается при вводе символа ‘q’ или при вводе числа, сумма цифр которого чётная.
+*/
+
+using System.Globalization;
+
+int RequestDigit(int msg)
+{
+Console.WriteLine (msg);
+int numbers = Convert.ToInt32(Console.ReadLine());
+while (numbers % 1 == 0)
+    {
+        int result = numbers;
+    }
+return result;
+}
+Console.WriteLine("Число не является целым, повторите ввод");
+
+
+
+
