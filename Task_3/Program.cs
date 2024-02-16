502d78f24dcf8007019d716b307d9eb126f4e0c9
Yuriy Mikhaylov <yuriy_vm@inbox.ru>
2024-02-16 15:02:07 +0300

    Complete Task_3

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -1,3 +1,43 @@
 ﻿/*
-
+Напишите программу, которая перевернёт одномерный массив
+(первый элемент станет последним, второй – предпоследним и т.д.)
 */
+
+void Main()
+{
+    Console.Write("Введите длину массива: ");
+    int size = Convert.ToInt32(Console.ReadLine());
+    int[] array = GenerateArray(size);
+    Console.WriteLine("");
+    Console.WriteLine("Перевернутый массив:");    
+    int i = 0;
+    while (i < array.Length / 2)
+    {
+       int temp = array[i];
+       array [i] = array [size - 1 - i];
+       array [size - 1 - i] = temp;
+       i++;
+    }
+    foreach (int e in array)
+    {
+        Console.Write($"{e} ");
+    }
+
+    int[] GenerateArray(int arraySize)
+    {
+        int[] myArray = new int[arraySize];
+        Random random = new Random();
+        for (int i = 0; i < arraySize; i++)
+        {
+            int randomNumber = random.Next(100, 1000);
+            myArray[i] = randomNumber;
+        }
+        for (int i = 0; i < arraySize; i++)
+        {
+            Console.Write($"{myArray[i]} ");
+        }
+        return myArray;
+    }
+}
+
+Main();
